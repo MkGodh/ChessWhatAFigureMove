1dfb58c5398abfad9bc90efc4d1ee4dea8d372f8
MkGodh <fenryrvla@yandex.ru>
2024-04-10 13:24:58 +0300

    Добавьте файлы проекта.

---
+++ ChessWhatAFigureMove/ChessWhatAFigureMove.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int main(){
+	using namespace std;
+
+	cout << "input starter coordinates: \n";
+	string starterx, movingy;
+	cin >> starterx >> movingy;
+	
+	
+	if ((abs(starterx[0] - movingy[0]) == 1) && (abs(starterx[1] - movingy[1]) == 2) ||
+	    (abs(starterx[0] - movingy[0]) == 2) && (abs(starterx[1] - movingy[1]) == 1)) {
+	    cout << "this is horse move.\n";
+	}
+	if ((abs(starterx[0] - movingy[0]) == 0) && (abs(starterx[1] - movingy[1]) > 0) ||
+	    (abs(starterx[1] - movingy[1]) > 0) && (abs(starterx[0] - movingy[0]) == 0)) {
+	    cout << "this is rook move.\n";
+	}
+	if ((abs(starterx[0] - movingy[0]) == abs(starterx[1] - movingy[1])) &&
+	    (abs(starterx[0] - movingy[0 > 0]))) {
+	    cout << "this is elephant move.\n";
+	}if ((abs(starterx[0] - movingy[0]) == abs(starterx[1] - movingy[1])) &&
+	    (abs(starterx[0] - movingy[0 > 0])) ||
+	    (abs((starterx[0] - movingy[0]) == 0) && (abs(starterx[1] - movingy[1]) > 0) ||
+	        (abs(starterx[0] - movingy[0]) > 0) && (abs(starterx[1] - movingy[1]) == 0))) {
+	    cout << "this is queen move.\n";
+	}
+	if ((abs(starterx[0] - movingy[0]) <= 1 && abs(starterx[1] - movingy[1]) <= 1)) {
+	    cout << "this is king move.\n";
+	
+	}
+}
+
